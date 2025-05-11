1aaf9aa5d33e7b0b313a4b58d4eeb5e65d83f6e7
Sandy <SandeepKhandelwal.It@Gmail.com>
2025-05-11 11:08:39 -0400

    Changing user_id field from Integer to String

--- app/models/locations.py
+++ app/models/locations.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, Float, DateTime, func
+from sqlalchemy import Column, Integer, Float, DateTime, func, String
 from app.database.session import Base
 
 class UserLocation(Base):
     __tablename__ = 'user_locations'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(String, nullable=False)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     timestamp = Column(DateTime(timezone=True),server_default=func.now())
